11869b601a5a1b7d8f3f84d0932fed62657a9547
Saraesp <saraesposito145@gmail.com>
2022-12-13 16:31:33 +0100

    esercizio senza bonus

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function generaGrid()
+{
+    document.querySelector('.grid').innerHTML = '';
+    for(let i = 0; i < 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('square');
+        cell.innerText = i + 1;
+        cell.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            console.log(this.innerText);
+        });
+        document.querySelector('.grid').appendChild(cell);
+    }
+}
+
+document.getElementById('play-game').addEventListener('click', function(){
+    generaGrid();
+})
